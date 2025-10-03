c2dcf37040c90ffa86a9ffcc0e8fd949fbf92235
Dhinesh B <145217410+Dhinesh31125@users.noreply.github.com>
2025-10-03 22:06:10 +0530

    changes made on outpass by dhinesh

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Outpass from './components/student/Outpass'
+
+const App = () => {
+  return (
+    <div>
+        <Outpass />
+    </div>
+  )
+}
+
+export default App
