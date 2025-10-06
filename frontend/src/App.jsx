8f20deb592cf701692b8d135fcf8b048daa5d9c3
dhanesh <dhaneshprabhupro@gmail.com>
2025-10-06 22:49:02 +0530

    Dhanesh

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import HodHistoryPage from './components/faculty/hod/HodHistoryPage'
 import HodNavbar from './components/faculty/hod/HodNavbar'
 import OSNavbar from './components/office-staff/OSNavbar'
+import CounsellorODApproval from './components/faculty/counsellor/counsODApproval/CounsellorODApproval'
+import PrintPage from './components/faculty/office-assistant/PrintPage'
 
 const App = () => {
   return (
     <div>
       {/* <HodHistoryPage />   */}
       {/* <HodNavbar /> */}
-      <OSNavbar />
+      {/* <OSNavbar /> */}
+      {/* <CounsellorODApproval /> */}
+      <PrintPage />
     </div>
   )
 }
