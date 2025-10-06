b05c92ab7dbbaf9fa9b636e129140a294c69041e
dhanesh <dhaneshprabhupro@gmail.com>
2025-10-06 19:02:26 +0530

    The mass

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HodHistoryPage from './components/faculty/hod/HodHistoryPage'
+import HodNavbar from './components/faculty/hod/HodNavbar'
+import OSNavbar from './components/office-staff/OSNavbar'
+
+const App = () => {
+  return (
+    <div>
+      {/* <HodHistoryPage />   */}
+      {/* <HodNavbar /> */}
+      <OSNavbar />
+    </div>
+  )
+}
+
+export default App
